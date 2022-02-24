390725278f2a491539ce62824425e9f6920a46b2
Vinicius Sueiro <hi@vsueiro.com>
2022-02-24 10:26:02 -0300

    Add code that uses JSON with better data structure

---
+++ salario-json/app.js
@@ -0,0 +1,29 @@
+let salaries = []
+
+fetch( 'salaries.json' )
+  .then( response => response.json() )
+  .then( data     => salaries = data )
+
+function calculate( event ) {
+
+  event.preventDefault()
+  const value = parseInt( input.value )
+
+  for ( let salary of salaries ) {
+
+    console.log( value, salary.lowerLimit )
+
+    if ( value  salary.lowerLimit ) {
+      show( salary.percentile )
+      break
+    }
+  }
+
+}
+
+function show( percentile ) {
+  output.textContent = percentile
+  progress.value = percentile
+}
+
+form.addEventListener( 'submit', calculate )
