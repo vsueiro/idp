5422f5748b9c54da4acc57579d94a75ac8867553
Vinicius Sueiro <hi@vsueiro.com>
2022-02-25 10:56:33 -0300

    Remove previous calculator versions

--- salario/app.js
+++ salario/app.js
@@ -1,82 +1,26 @@
-// Declare global variables
+let salaries = []
 
-let input     = document.querySelector( 'input' )
-let button    = document.querySelector( 'button' )
-let output    = document.querySelector( '.output' )
-let fill      = document.querySelector( '.fill' )
-let highlight = document.querySelector( '.highlight' )
-let form      = document.querySelector( 'form' )
-let salaries
+fetch( 'salaries.json' )
+  .then( response => response.json() )
+  .then( data     => salaries = data )
 
+function calculate( event ) {
 
+  event.preventDefault()
+  const value = parseInt( input.value )
 
-// Update results
-
-function update( event ) {
-
-  if ( event )
-    event.preventDefault()
-
-  // Cast input, from string to number
-  let value = parseInt( input.value )
-
-  // Check if input is valid
-  if ( value > 0 ) {
-
-    // Show highlight only after user input
-    highlight.classList.add( 'visible' )
-
-    // Begin from lowest 1% of salaries
-    let percentil = 0
-
-    // Loop through all 100 salary ranges
-    for ( let salary of salaries ) {
-
-      // Check for upper limit
-      if ( salary == 'Infinity' )
-        salary = Infinity
-
-      // Check salary match
-      if ( value < salary ) {
-
-        // Update text
-        output.textContent = 'Você ganha mais que ' +  percentil + '% dos brasileiros:'
-
-        // Update bar chart
-        fill.style.width = percentil + '%'
-        highlight.style.left = percentil + '%'
-
-        // Look no further
-        break
-
-      }
-      
-      // Increment 
-      percentil++
-
+  for ( let salary of salaries ) {
+    if ( value > salary.lowerLimit ) {
+      show( salary.percentile )
+      break
     }
-
-  } else {
-
-    // Handle input error
-    alert( 'Insira um salário válido' )
-
   }
 
 }
 
+function show( percentile ) {
+  output.textContent = percentile
+  progress.value = percentile
+}
 
-
-// Load data
-
-fetch( 'data.json' )
-  .then( response => response.json() )
-  .then( data => {
-
-    // Assign data to global variable
-    salaries = data
-
-    // Handle form submission (works with Enter key)
-    form.addEventListener( 'submit', update )
-
-  } )
+form.addEventListener( 'submit', calculate )
