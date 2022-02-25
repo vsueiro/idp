d5138d727ad6ed4079fd1a625f1bce36da33dfef
Vinicius Sueiro <hi@vsueiro.com>
2022-02-25 12:11:19 -0300

    Start creating of embed version (with CSS styles)

---
+++ salario-embed/app.js
@@ -0,0 +1,30 @@
+let salaries = []
+
+fetch( 'salaries.json' )
+  .then( response => response.json() )
+  .then( data     => salaries = data )
+
+function calculate( event ) {
+  
+  event.preventDefault()
+  const value = parseInt( input.value )
+
+  for ( let salary of salaries ) {
+    if ( value > salary.lowerLimit ) {
+      show( salary.percentile )
+      break
+    }
+  }
+
+}
+
+function show( percentile = '' ) {
+  output.textContent = percentile + '%'
+}
+
+function clear() {
+  output.textContent = '…%'
+}
+
+input.addEventListener( 'keydown', clear )
+form.addEventListener( 'submit', calculate )
