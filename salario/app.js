0b52605663630364963f56fe3ccbaf174c48775c
Vinicius Sueiro <hi@vsueiro.com>
2022-03-22 17:33:32 -0300

    Make chart with discrete bins (to be highlighted)

--- salario/app.js
+++ salario/app.js
@@ -3,8 +3,43 @@ let salaries = []
 
 fetch( file )
   .then( response => response.json() )
-  .then( data => salaries = data.reverse() )
+  .then( data => {
 
+    salaries = data
+    createBins( salaries )
+    salaries.reverse()
+
+  } )
+
+function createBins( salaries ) {
+  for ( let salary of salaries ) {
+    let size = salary.to - salary.from
+    let bin = document.createElement( 'div' )
+    bin.style.width = size + '%'
+    bins.appendChild( bin )
+  }
+}
+
+function highlightBin( number ) {
+
+  let all = document.querySelectorAll( '#bins > div' )
+  let index = 0
+
+  for ( let bin of all ) {
+
+    bin.classList.remove( 'you', 'on' )
+
+    if ( index < number )
+      bin.classList.add( 'on' )
+
+    if ( index === number )
+      bin.classList.add( 'you' )
+
+    index++
+  }
+
+}
+ 
 function validate() {
 
   const value = parseInt( input.value )
@@ -16,32 +51,47 @@ function validate() {
 
 }
 
+function show( percentile ) {
+  output.textContent = percentile + '%'
+}
+
 function calculate( value ) {
 
+  let index = 0
+
   for ( let salary of salaries ) {
+
     if ( value > salary.lowerLimit ) {
-      show( salary.from )
-      resizeBin( salary )
+
+      let percentile = salary.from
+      let number = salaries.length - index - 1
+
+      if ( number === 0 ) {
+        clear()
+      } else {
+        highlightBin( number )
+        show( percentile )
+      }
+
       break
+
     }
+    index++
   }
 
 }
 
-function show( percentile ) {
-  output.textContent = percentile + '%'
-  fill.style.width = percentile + '%'
-}
-
-function resizeBin( salary ) {
-  let size = salary.to - salary.from
-  bin.style.width = size + '%'
-}
-
 function clear() {
   output.textContent = '…%'
-  fill.removeAttribute( 'style' )
-  bin.removeAttribute( 'style' )
+
+  let all = document.querySelectorAll( '#bins > div' )
+  let index = 0
+
+  for ( let bin of all ) {
+    bin.classList.remove( 'you', 'on' )
+    index++
+  }
+
 }
 
 input.addEventListener( 'input', validate )
